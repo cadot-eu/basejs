e643a840c2aeadf272a967f2f476d325686d6848
Michael <m@cadot.eu>
2022-06-09 18:50:43 +0200

    maj img alt

--- alt_for_img.js
+++ alt_for_img.js
@@ -3,6 +3,11 @@
 see if the alt attribute is undefined. If it is, it sets the alt attribute to the filename of the
 image. */
 $("img").each(function () {
-    if ($(this).attr('alt') == undefined)
-        $(this).attr('alt', $(this).attr('src').split('/').reverse()[0].split('.')[0].replace('-', ' ').replace('_', ' '))
+    if ($(this).attr('alt') == undefined) {
+        let source = null;
+        if ($(this).attr('src') != 'undefined') source = $(this).attr('src');
+        if ($(this).attr('data-bp') != 'undefined') source = $(this).attr('data-bp');
+        if (source != null)
+            $(this).attr('alt', source.split('/').reverse()[0].split('.')[0].replace('-', ' ').replace('_', ' '))
+    }
 })
