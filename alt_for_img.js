2795f1b425d3d98175a598cc68d98184e902bb16
Michael <m@cadot.eu>
2022-06-09 22:23:44 +0200

    fix alt

--- alt_for_img.js
+++ alt_for_img.js
@@ -2,12 +2,12 @@
 /* This is a JavaScript function that is being called on each image in the document. It is checking to
 see if the alt attribute is undefined. If it is, it sets the alt attribute to the filename of the
 image. */
-$("img").each(function () {
-    if ($(this).attr('alt') == undefined) {
+document.querySelectorAll('img').forEach(el => {
+    if (el.getAttribute('alt') == null) {
         let source = null;
-        if ($(this).attr('src') != 'undefined') source = $(this).attr('src');
-        if ($(this).attr('data-bp') != 'undefined') source = $(this).attr('data-bp');
+        if (el.getAttribute('src')) source = el.getAttribute('src');
+        if (el.getAttribute('data-bp')) source = el.getAttribute('data-bp');
         if (source != null)
-            $(this).attr('alt', source.split('/').reverse()[0].split('.')[0].replace('-', ' ').replace('_', ' '))
+            el.setAttribute('alt', source.split('/').reverse()[0].split('.')[0].replace('-', ' ').replace('_', ' '))
     }
-})
+});
