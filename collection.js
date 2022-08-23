239d4765aa77d684881930fecfb4b90942e6bff0
Michael <m@cadot.eu>
2022-08-23 14:50:08 +0200

    debut creer comparatif

--- collection.js
+++ collection.js
@@ -3,8 +3,23 @@ import a2lix_lib from '@a2lix/symfony-collection'
 a2lix_lib.sfCollection.init({
     collectionsSelector: 'form div[data-prototype]',
     manageRemoveEntry: true,
-    lang: {
-        add: 'Ajouter',
-        remove: 'Supprimer'
+    entry: {
+        add: {
+            prototype:
+                '<button class="__class__" data-entry-action="add">__label__</button>',
+            class: 'btn btn-primary btn-sm mt-2',
+            label: 'ajouter',
+            customFn: null,
+            onBeforeFn: null,
+            onAfterFn: null
+        },
+        remove: {
+            prototype:
+                '<button class="__class__" data-entry-action="remove">__label__</button>',
+            class: 'btn btn-danger btn-sm',
+            label: 'Surpprimer',
+            customFn: null,
+            onAfterFn: null
+        }
     }
 })
