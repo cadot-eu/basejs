e526a8c3f908451b20ec8371b756b86cd78996c0
Michael <m@cadot.eu>
2022-11-30 10:38:27 +0100

    création de simplebox avec fichier templates exterieur

--- simplebox/simpleboxediting.js
+++
@@ -1,7 +0,0 @@
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-
-export default class SimpleBoxEditing extends Plugin {
-    init() {
-        console.log( 'SimpleBoxEditing#init() got called' );
-    }
-}
--- simplebox/simpleboxui.js
+++
@@ -1,7 +0,0 @@
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-
-export default class SimpleBoxUI extends Plugin {
-    init() {
-        console.log( 'SimpleBoxUI#init() got called' );
-    }
-}
