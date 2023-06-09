629070897c747cb16a99e48624a2c5d06f349a25
michael <m@cadot.eu>
2023-06-09 09:10:16 +0200

    add compte et alerte en flash pour click sur id

--- flash_message.js
+++ flash_message.js
@@ -1,3 +1,32 @@
-$("#flash_message").fadeTo(5000, 500).slideUp(500, function () {
-    $("#flash_message").slideUp(500);
-});
+import Swal from 'sweetalert2'
+let timerInterval
+function flash_message(type, title, html = false, toast = true, timer = 2000) {
+    Swal.fire({
+        icon: type,
+        title: title,
+        html: html,
+        timer: timer,
+        timerProgressBar: true,
+        toast: toast,
+
+
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+        },
+        didOpen: () => {
+            Swal.showLoading()
+            const b = Swal.getHtmlContainer().querySelector('b')
+        },
+        willClose: () => {
+            clearInterval(timerInterval)
+        }
+    }).then((result) => {
+        if (result.dismiss === Swal.DismissReason.timer) {
+            //console.log('I was closed by the timer')
+        }
+    })
+}
+export default flash_message;
